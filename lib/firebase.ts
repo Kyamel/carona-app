92410794d280d9c899eef62322b07bfc5172357e
Kyamel <lcamelo200@gmail.com>
2026-07-09 14:37:49 -0300

    chore: setup firebase auth and firestore db and static spa hosting

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,21 +1,24 @@
 // Import the functions you need from the SDKs you need
-import { getAnalytics } from "firebase/analytics";
-import { initializeApp } from "firebase/app";
+import { getApp, getApps, initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC5rZDQX4KavJQTLx9igow2nWEqutLrIQk",
-  authDomain: "carona-app-4e8f6.firebaseapp.com",
-  projectId: "carona-app-4e8f6",
-  storageBucket: "carona-app-4e8f6.firebasestorage.app",
-  messagingSenderId: "824734974401",
-  appId: "1:824734974401:web:e4e08e55d328287f176f40",
-  measurementId: "G-83V3RBY4W5",
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+  //measurementId: "G-83V3RBY4W5",
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
